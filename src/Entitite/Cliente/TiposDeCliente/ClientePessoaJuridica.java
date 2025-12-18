86d0fac979cc09e51d3864b1d35d1c4681db67d1
Matheus Eugênio Granja <eugeniomatheus977@gmail.com>
2025-12-18 17:07:21 -0300

    atualização no construtor da classe ClientePessoaFisica.java

--- src/Entitite/Cliente/TiposDeCliente/ClientePessoaJuridica.java
+++ src/Entitite/Cliente/TiposDeCliente/ClientePessoaJuridica.java
@@ -1,16 +1,23 @@
 package Entitite.Cliente.TiposDeCliente;
 
 import Entitite.Cliente.Cliente;
+import Entitite.Cliente.Endereco;
 
 public class ClientePessoaJuridica extends Cliente {
 
     private String cnpj;
     private String nome_Empresa;
 
-    public ClientePessoaJuridica(String nome, String endereco, String cnpj, String nome_Empresa, String dataNascimento) {
+    public ClientePessoaJuridica(String nome, Endereco endereco, String cnpj, String nome_Empresa, String dataNascimento) {
         super(nome, endereco, dataNascimento);
         this.cnpj = cnpj;
         this.nome_Empresa = nome_Empresa;
+    }
+
+    public ClientePessoaJuridica(String nome, Endereco endereco, String cnpj, String dataNascimento) {
+        super(nome, endereco, dataNascimento);
+        this.cnpj = cnpj;
+        this.nome_Empresa = "Não têm";
     }
 
     public String getCnpj() {return cnpj;}
@@ -18,6 +25,6 @@ public class ClientePessoaJuridica extends Cliente {
 
     @Override
     public String toString() {
-        return "ClientePessoaJuridica - "+ super.toString() +", cnpj - " + cnpj + ", Nome da Empresa atrelada ao CNPJ - " + nome_Empresa;
+        return "ClientePessoaJuridica - "+ super.toString() +", CNPJ - " + cnpj + ", Nome da Empresa atrelada ao CNPJ - " + nome_Empresa;
     }
 }
