90a878167e26a658db8bb16c7111cf1f6767dd64
Matheus Eugênio Granja <eugeniomatheus977@gmail.com>
2025-12-18 15:11:04 -0300

    Atualizações básicas e simples

--- src/teste/Main.java
+++ src/teste/Main.java
@@ -6,10 +6,10 @@ import Entitite.Conta.Personas_Da_Conta.ContaPoupanca;
 
 public class Main {
     public static void main(String[] args) {
-        ClientePessoaFisica cPF = new ClientePessoaFisica("cleiton", 133, "Rua 123", "12/12/2003");
-        Conta c1 = new ContaPoupanca(12, 2, cPF, 12.0);
-
-        cPF.vincularConta(c1);
-        cPF.consultarContas();
+//        ClientePessoaFisica cPF = new ClientePessoaFisica("cleiton", 133, "Rua 123", "12/12/2003");
+//        Conta c1 = new ContaPoupanca(12, 2, cPF, 12.0);
+//
+//        cPF.vincularConta(c1);
+//        cPF.consultarContas();
     }
 }
