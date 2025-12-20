cc9df48c89d5e1de2b2f3f14ab78b96883e26ff5
Matheus Eugênio Granja <eugeniomatheus977@gmail.com>
2025-12-20 16:45:55 -0300

    atualização da exception InvalidValueException

--- src/entity/InvalidValueException.java
+++ src/entity/InvalidValueException.java
@@ -4,4 +4,5 @@ public class InvalidValueException extends Exception {
     public InvalidValueException(String message) {
         super(message);
     }
+    public InvalidValueException() {super();}
 }
