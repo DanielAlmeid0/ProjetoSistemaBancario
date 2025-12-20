18457276ab377303d84f6006fe2155c3ad018998
Matheus Eugênio Granja <eugeniomatheus977@gmail.com>
2025-12-20 16:49:49 -0300

    adiçao da Main para testes dos métodos da classe Banco.java

--- src/teste/Main.java
+++ src/teste/Main.java
@@ -1,7 +1,30 @@
 package teste;
 
+import entity.InvalidValueException;
+import service.Banco;
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Scanner sc = new  Scanner(System.in);
+        Banco banco = new Banco("Banco do Brasil" , "12343");
+
+        try {
+
+            System.out.println("Quantos clientes deseja cadastrar?");
+            int quant = sc.nextInt();
+            sc.nextLine();
+
+            for (int i = 0; i < quant; i++) {
+                banco.adicionarCliente(sc);
+            }
+
+            banco.mostrarClientesDoBanco();
+        }catch (InvalidValueException e){
+            System.out.println("ERRO: "+e.getMessage());
+        }
+
 //        ClientePessoaFisica cPF = new ClientePessoaFisica("cleiton", 133, "Rua 123", "12/12/2003");
 //        Conta c1 = new ContaPoupanca(12, 2, cPF, 12.0);
 //
